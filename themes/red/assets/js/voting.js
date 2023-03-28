0e7bd2d0cac33a0c38fdf46802ee617dcc67a3d5
Joshua Goins <josh@redstrate.com>
2023-03-28 12:53:02 -0400

    Add voting system

---
+++ themes/red/assets/js/voting.js
@@ -0,0 +1,18 @@
+function refresh(slug) {
+        fetch('https://voting.redstrate.com/votes/view/' + slug)
+        .then(function(response) {
+                return response.json();
+        })
+        .then(function(data) {
+                console.log(data)
+                document.getElementById("num-votes").innerHTML = data["votes"] + " votes";
+        });
+}
+
+function addVote(slug) {
+        const request = new Request("https://voting.redstrate.com/votes/submit/" + slug, {
+                method: "POST"
+        });
+        fetch(request)
+        .then((response) => { refresh(slug) });
+}
