a56cfba95c6bd572168f699958620215a36e455d
Joshua Goins <josh@redstrate.com>
2024-02-19 15:38:21 -0500

    Fix navigation menu closing too often

--- themes/red/assets/js/site.js
+++ themes/red/assets/js/site.js
@@ -1,7 +1,5 @@
 function checkWidth() {
-    if (window.matchMedia('(max-device-width: 768px)').matches) {
-        document.getElementById("nav-menu").open = false;
-    } else {
+    if (!window.matchMedia('(max-device-width: 768px)').matches) {
         document.getElementById("nav-menu").open = true;
     }
 }
